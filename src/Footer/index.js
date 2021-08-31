a54c792c9f68e8d58e8f6f4507e98446aa9dccb1
Beicker Zambrano <beicker998@gmail.com>
2021-08-30 19:04:02 -0500

    feat:add slider

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
 
 export default function Footer() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [sliderRef, slider] = useKeenSlider({
+    initial: 0,
+    slideChanged(s) {
+      setCurrentSlide(s.details().relativeSlide);
+    },
+  });
+
   return (
     <div className={styles.Wrapper}>
       <div>
@@ -32,6 +43,56 @@ export default function Footer() {
           src="https://vrea.usm.cl/wp-content/uploads//2021/06/logo-acreditacion_blanco.svg"
         />
       </div>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          <div className={`keen-slider__slide number-slide1 ${styles.logo}`}>
+            <img src="https://vrea.usm.cl/wp-content/uploads/2021/04/logo_reuna.svg" />
+          </div>
+          <div className={`keen-slider__slide number-slide2 ${styles.logo}`}>
+            <img src="https://vrea.usm.cl/wp-content/uploads/2021/04/logo_reuna.svg" />
+          </div>
+          {slider && (
+            <>
+              <ArrowLeft
+                onClick={(e) => e.stopPropagation() || slider.prev()}
+                disabled={currentSlide === 0}
+              />
+              <ArrowRight
+                onClick={(e) => e.stopPropagation() || slider.next()}
+                disabled={currentSlide === slider.details().size - 1}
+              />
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
+const ArrowLeft = (props) => {
+  const disabled = props.disabled ? ` ${styles["arrow--disabled"]}` : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`${styles.arrow} ${styles["arrow--left"]}` + disabled}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+    </svg>
+  );
+};
+
+const ArrowRight = (props) => {
+  const disabeld = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`${styles.arrow} ${styles["arrow--right"]}` + disabeld}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+    </svg>
+  );
+};
